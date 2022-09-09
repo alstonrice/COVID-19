b5cedb63c4154580ce10f68aa0bca6362ad0d92f
alstonrice <alston.rice@gmail.com>
2022-09-09 18:14:51 -0400

    ExplanitoryData

--- Folder_R/ExplanitoryDataTest.R
+++ Folder_R/ExplanitoryDataTest.R
@@ -34,39 +34,48 @@ CovidBit <- DataByDate %>%
   select(Date,Close,dailyActive,dailyConfirmed,dailyDeaths,time,month,year)
 
 library(corrgram)
-
+###### adding Incident
 CovidRate <- data %>%
   filter(date > mdy("01/01/2020") & date < mdy("09/01/2022")) %>%
   rename(Date = date) %>% 
-    mutate(Confirmed = ifelse(is.na(Confirmed),0,Confirmed),
+  mutate(Confirmed = ifelse(is.na(Confirmed),0,Confirmed),
            Active = ifelse(is.na(Active),0,Active),
            Deaths = ifelse(is.na(Deaths),0,Deaths),
            Recovered = ifelse(is.na(Recovered),0,Recovered),
            Incident_Rate = ifelse(is.na(Incident_Rate),0,Incident_Rate),
-           Country_Region = if_else(Country_Region=="Mainland China","China",Country_Region))%>%
+           Country_Region = case_when(Country_Region=="Mainland China" ~ "China",
+                                    TRUE ~ Country_Region))%>%
   # code to summarise() up to one row per day
   group_by(Country_Region,Date) %>%
   summarise(Fatality_Rate = sum(Deaths)/ sum(Confirmed)*100,
             Confirmed = sum(Confirmed),
             Deaths = sum(Deaths),
             Incident_Rate = sum(Incident_Rate)) %>% 
-  full_join(.,btcData)
-
+  full_join(.,btcData) %>%filter(!is.na(Confirmed))
+ 
+save(CovidRate,"./rdsData/ExplanitoryDataTest.rds")
 
 ###creating variables for the day, month, and year
 
+Bc <- readRDS("./rdsData/ExplanitoryDataTest.rds")
+
+library(lubridate)
+library(tidyverse)
+
+source("./r/readBitcionPrice.R")
+
 CovidRate %>%
-  group_by(Date) %>%
-  summarize(Confirmed = sum(Confirmed),
-            Indcident_Rate = mean(Indcident_Rate),
-            Fatality_Rate = sum(Deaths)/ sum(Confirmed)*100,
-            Death = sum(Deaths)) %>%
-  full_join(.,btcData) %>%
-  mutate(time = Date - myd("01/20/2020"),
+  group_by(Date) %>%  
+  summarise(Confirmed = sum(Confirmed),
+            Incident_Rate = mean(Indcident_Rate),
+            Death = sum(Deaths),
+            Fatality_Rate = sum(Deaths)/ sum(Confirmed*100)) %>%
+  full_join(.,btcData) %>% filter(!is.na(Confirmed)) %>%
+  mutate(time = Date - myd("01/21/2020"),
          month = month(Date),
          year = year(Date),
          dailyConfirmed = if_else(Date == mdy("01/22/2022"),Confirmed,Confirmed - lag(Confirmed)),
-         dailyDeaths = if_else(Date == mdy("01/22/2022"),Deaths,Deaths - lag(Deaths)))
+         dailyDeaths = if_else(Date == mdy("01/22/2022"),Deaths,Deaths - lag(Deaths)))-> TestCovBit
 
 ##modeling
 
